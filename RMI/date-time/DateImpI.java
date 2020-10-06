b7cf6fffb16beeac139c4bc92ddb2142f5a6d805
disha-khatri <disha-khatri@users.noreply.github.com>
2020-10-06 17:43:21 +0530

    DateTime

---
+++ RMI/date-time/DateImpI.java
@@ -0,0 +1,11 @@
+import java.rmi.*;
+import java.rmi.server.*;
+import java.util.*;
+public class DateImpl extends UnicastRemoteObject implements IDate
+{
+public DateImpl() throws RemoteException{}
+public String getDate(){
+Date d=new Date();
+return(d.toString());
+}
+}
---
+++ RMI/date-time/IDate.java
@@ -0,0 +1,6 @@
+import java.rmi.*;
+
+public interface IDate extends Remote
+{
+String getDate() throws RemoteException;
+}
